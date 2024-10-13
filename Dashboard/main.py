299350c219d77fb013bdd05b1477adb36adae87a
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-10-13 21:55:17 +0100

    add redis to dashboard and add redis-data to  gitignore, add mapping logic in daata-transformation srrvice

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -1,9 +1,9 @@
-import time
-import streamlit as st
-from utils.database import init_mongo,get_dests_from_mongo
-from utils.scraping import get_departs,get_tarifs,wait_for_spider
-from utils.ETL import clean_filter
 import pandas as pd
+import streamlit as st
+from utils.database import get_data, init_mongo
+from utils.ETL import clean_filter
+from utils.scraping import get_departs, get_tarifs, wait_for_spider
+
 st.set_page_config(page_title="TuGo", page_icon="🚗", layout="wide")
 # Custom HTML and CSS for the footer
 footer = """
@@ -43,87 +43,122 @@ footer = """
 st.markdown(footer, unsafe_allow_html=True)
 
 
-collection , dests,tarif_collection = init_mongo()
-raw_data,data = clean_filter(collection,{})
+collection, dests, tarif_collection = init_mongo()
+raw_data, data = clean_filter(collection, {})
 st.title("TuGo - Move with ease and confort 🚗")
-st.write("This is a simple web application that facilitates your transportation in Tunisia")
+st.write(
+    "This is a simple web application that facilitates your transportation in Tunisia"
+)
 st.write("The data is fetshed directly from the companies **Official** websites")
 selected_companies = st.multiselect("Select Company", ["SRTM", "SRTG", "Soretras"])
 if selected_companies:
     filter = {"Company": {"$in": selected_companies}} if selected_companies else {}
-    raw_data,data = clean_filter(collection,filter)
+    raw_data, data = clean_filter(
+        collection,
+        filter,
+    )
     if len(data) == 0:
-        st.info("No data available for the selected companies We will get the data from the web")
+        st.info(
+            "No data available for the selected companies We will get the data from the web"
+        )
         with st.spinner("Getting data from the web ..."):
             get_departs()
-            raw_data,data = clean_filter(collection,filter)
-            selected_departure = st.selectbox("Select Departure Station", data,key="depart")
+            raw_data, data = clean_filter(collection, filter)
+            selected_departure = st.selectbox(
+                "Select Departure Station", data, key="depart"
+            )
             if selected_departure:
-                
-                selected_dict = [i for i in raw_data if i["Name"]==selected_departure]
+                selected_dict = [i for i in raw_data if i["Name"] == selected_departure]
                 if len(selected_dict) == 0:
                     st.info(selected_dict)
                 else:
                     selected_dict = selected_dict[0]
-                raw_dests,clean_dests = get_dests_from_mongo(dests, selected_dict, selected_companies)
+                    filter = (
+                        {
+                            "Depart": selected_departure["Name"],
+                            "Company": {"$in": selected_companies},
+                        }
+                        if selected_companies
+                        else {"Depart": selected_departure}
+                    )
+                raw_dests, clean_dests = get_data(
+                    "destination:1", dests, selected_dict, filter
+                )
                 selected_dests = st.selectbox("Select Destination", clean_dests)
     else:
-        selected_departure = st.selectbox("Select Departure Station", data,key="depart")
+        selected_departure = st.selectbox(
+            "Select Departure Station", data, key="depart"
+        )
         if selected_departure:
-            selected_dict = [i for i in raw_data if i["Name"]==selected_departure]
+            selected_dict = [i for i in raw_data if i["Name"] == selected_departure]
             if len(selected_dict) == 0:
                 st.info(selected_dict)
             else:
                 selected_dict = selected_dict[0]
-            raw_dests,clean_dests = get_dests_from_mongo(dests, selected_dict, selected_companies)
+            raw_dests, clean_dests = get_data(
+                "destination:1", dests, selected_dict, selected_companies
+            )
             selected_dest = st.selectbox("Select Destination", clean_dests)
-            
-            if selected_dest :
-                selected_dest_dicts = [i for i in raw_dests if i["Name"]==selected_dest]
+
+            if selected_dest:
+                selected_dest_dicts = [
+                    i for i in raw_dests if i["Name"] == selected_dest
+                ]
                 if len(selected_dest_dicts) == 0:
                     st.info("No data available for the selected destination")
                 else:
                     with st.spinner("Getting Tarification from the web ..."):
                         selected_dest_dict = selected_dest_dicts[0]
-                        available_tarifs = list(tarif_collection.find({"depart":selected_departure,"destination":selected_dest}, {"_id":0}))
-                        if len(available_tarifs) == 0:   
-                            response = get_tarifs(selected_dict, selected_dest_dict, selected_dest_dict["Company"])
+                        available_tarifs = list(
+                            tarif_collection.find(
+                                {
+                                    "depart": selected_departure,
+                                    "destination": selected_dest,
+                                },
+                                {"_id": 0},
+                            )
+                        )
+                        if len(available_tarifs) == 0:
+                            response = get_tarifs(
+                                selected_dict,
+                                selected_dest_dict,
+                                selected_dest_dict["Company"],
+                            )
                             wait_for_spider(response)
-                            filter = {"depart":selected_departure,"destination":selected_dest}
-                            _,_,tarif_collection = init_mongo()
-                            l=list(tarif_collection.find(filter,{"_id":0}))
-                            df = pd.DataFrame(l)
+                            filter = {
+                                "depart": selected_departure,
+                                "destination": selected_dest,
+                            }
+                            _, _, tarif_collection = init_mongo()
+                            list_tarifs = list(
+                                tarif_collection.find(filter, {"_id": 0})
+                            )
+                            df = pd.DataFrame(list_tarifs)
                         else:
-                            l=list(available_tarifs)
-                            df = pd.DataFrame(l)
+                            list_tarifs = list(available_tarifs)
+                            df = pd.DataFrame(list_tarifs)
                         with st.container():
-                            st.dataframe(df,hide_index=True,use_container_width=True)
+                            st.dataframe(df, hide_index=True, use_container_width=True)
 
-                        st.info("S'il ya une fausse donnée ou une donné monquante, veuillez les signaler\nou l'ajouter\nNous avons besoin de votre aide pour améliorer notre service\nMerci pour votre compréhension\nNous allons vérifier votre demande et vous répondre dans les plus brefs délais")
+                        st.info(
+                            "S'il ya une fausse donnée ou une donné monquante, veuillez les signaler\nou l'ajouter\nNous avons besoin de votre aide pour améliorer notre service\nMerci pour votre compréhension\nNous allons vérifier votre demande et vous répondre dans les plus brefs délais"
+                        )
                         form_holder = st.form("form")
                         with form_holder:
-                            col_dep,col_dest = st.columns(2)
-                            col_dep.selectbox("Select Departure Station", data,key="depart_input")
-                            col_dest.selectbox("Select Destination", clean_dests,key="dest_input")
-                            coldep_time,colarr_time = st.columns(2)
-                            coldep_time.time_input("Departure Time",key="dep_time")
-                            colarr_time.time_input("Arrival Time",key="arr_time")
+                            col_dep, col_dest = st.columns(2)
+                            col_dep.selectbox(
+                                "Select Departure Station", data, key="depart_input"
+                            )
+                            col_dest.selectbox(
+                                "Select Destination", clean_dests, key="dest_input"
+                            )
+                            coldep_time, colarr_time = st.columns(2)
+                            coldep_time.time_input("Departure Time", key="dep_time")
+                            colarr_time.time_input("Arrival Time", key="arr_time")
                             colprice = st.columns(1)
-                            st.number_input("Price",key="price")
+                            st.number_input("Price", key="price")
                             colcompany = st.columns(1)
                             st.selectbox("Select Company", selected_companies)
                             submit = st.form_submit_button("Submit")
                         if submit:
                             st.write("Form submitted")
-   
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- Dashboard/utils/database.py
+++ Dashboard/utils/database.py
@@ -4,6 +4,9 @@ import os
 import redis
 import streamlit as st
 from pymongo import MongoClient
+from redis.commands.json.path import Path
+from redis.commands.search.field import NumericField, TextField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from utils.ETL import clean_filter
 from utils.scraping import get_available_destinations
 
@@ -11,6 +14,19 @@ from utils.scraping import get_available_destinations
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 mongo_uri = os.getenv("MONGO_URI")
+
+r = redis.Redis(host=redis_host, port=redis_port, db=0)
+schema = (
+    TextField("$.Depart", as_name="depart"),
+    NumericField("$.Id", as_name="id"),
+    TextField("$.Name", as_name="name"),
+    TextField("$.Company", as_name="company"),
+)
+r.ft().dropindex()
+r.ft().create_index(
+    schema,
+    definition=IndexDefinition(prefix=["destination:"], index_type=IndexType.JSON),
+)
 
 
 # @st.cache_resource(show_spinner=False)
@@ -23,12 +39,7 @@ def init_mongo():
     return collection, dests, tarif_collecion
 
 
-def get_dests_from_mongo(collection, selected_departure: str, selected_companies: list):
-    filter = (
-        {"Depart": selected_departure["Name"], "Company": {"$in": selected_companies}}
-        if selected_companies
-        else {"Depart": selected_departure}
-    )
+def get_dests_from_mongo(collection, selected_departure: str, filter):
     raw_dests, clean_dests = clean_filter(collection, filter)
     if len(clean_dests) == 0:
         with st.spinner("Getting data from the web ..."):
@@ -41,20 +52,23 @@ def get_dests_from_mongo(collection, selected_departure: str, selected_companies
     return raw_dests, clean_dests
 
 
-r = redis.Redis(host=redis_host, port=redis_port, db=0)
-
-
 # Example function to get data with caching logic
-def get_data(query, collection, selected_departure: str, selected_companies: list):
+def get_data(query, collection, selected_departure, filter):
     # Check if data is cached in Redis
     cached_data = r.get(query)
     if cached_data:
         return json.loads(cached_data)  # Return cached data
 
     # If not cached, fetch from MongoDB (implement this function)
-    data = get_dests_from_mongo(collection, selected_departure, selected_companies)
+    data = get_dests_from_mongo(collection, selected_departure, filter)
 
     # Cache the result in Redis for future requests (set expiration time if needed)
-    r.set(query, json.dumps(data), ex=3600)  # Cache for 1 hour
-
+    r.json().set("destination:1", Path.root_path(), data)  # Cache for 1 hour
+    cursor = 0
+    while True:
+        cursor, keys = r.scan(cursor)
+        for key in keys:
+            st.write(key)
+        if cursor == 0:
+            break
     return data
